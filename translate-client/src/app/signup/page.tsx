c2c9b1c7f121fe8a9b60113b89b32a45d34ae9f3
Luis De La Cruz <luisdanieldlcg@gmail.com>
2024-10-15 11:10:14 -0400

    client: create signup page

--- translate-client/src/app/signup/page.tsx
+++ translate-client/src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
-const LoginPage = () => {
+const SignupPage = () => {
   return (
     <div className="flex flex-col max-w-screen-sm mx-auto my-16 gap-5">
       <h1 className="text-center text-3xl mt-16 mb-12">translate.</h1>
@@ -13,15 +13,17 @@ const LoginPage = () => {
         Password
       </label>
       <input type="password" id="password" className="outlined-input" />
-      <button className="primary-button my-4">Login</button>
-      <div className="text-right">
-        Forgot your password?
-        <span className="font-bold"> Click to reset</span>
-      </div>
+
+      <label htmlFor="confirm-password" className="font-bold">
+        Confirm Password
+      </label>
+      <input type="password" id="confirm-password" className="outlined-input" />
+      <button className="primary-button my-4">Signup</button>
+
       <hr className="my-2 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400" />
       <div className="text-center">
-        Not a member yet? {""}
-        <Link className="font-bold" href="/signup">
+        Already have an account? {""}
+        <Link className="font-bold" href="/login">
           Click to login
         </Link>
       </div>
@@ -29,4 +31,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default SignupPage;
